ee7d742391d7b9147536dbee4ef63363e8d36751
Aaron Faucher <aaron.faucher@gmail.com>
2018-03-22 22:35:56 -0400

    New Tutorial
    https://blogs.msdn.microsoft.com/cdndevs/2017/04/28/uwp-working-with-bluetooth-devices-part-1/

--- BluetoothWatcher/MainPage.xaml.cs
+++ BluetoothWatcher/MainPage.xaml.cs
@@ -14,9 +14,9 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+
 using Windows.Devices.Bluetooth;
-using Windows.Devices.Bluetooth.Advertisement;
-using Windows.Devices.Bluetooth.GenericAttributeProfile;
+using Windows.Devices.Enumeration;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -27,54 +27,26 @@ namespace BluetoothWatcher
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        BluetoothLEAdvertisementWatcher watcher;
-        // public static ushort BEACON_ID = 1775;
 
         public MainPage()
         {
             this.InitializeComponent();
             Debug.WriteLine("Debug is working.");
 
+            // Initialize Device Picker
+            DevicePicker picker = new DevicePicker();
+            picker.Filter.SupportedDeviceSelectors.Add(BluetoothLEDevice.GetDeviceSelectorFromPairingState(false));
+            picker.Filter.SupportedDeviceSelectors.Add(BluetoothLEDevice.GetDeviceSelectorFromPairingState(true));
+            picker.Show(new Rect(0, 0, 200, 200));
+
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             Debug.WriteLine("Button Clicked");
-
-            if (watcher != null)
-                watcher.Stop();
             
-            watcher = new BluetoothLEAdvertisementWatcher();
-
-            // This is how we might filter the incoming Bluetooth
-            // Right now, we don't want to filter
-
-            //var manufacturerData = new BluetoothLEManufacturerData
-            //{
-            //    CompanyId = BEACON_ID
-            //};
-            // watcher.AdvertisementFilter.Advertisement.ManufacturerData.Add(manufacturerData);
-
-            watcher.Received += Watcher_Received;
-            watcher.Start();
         }
-
-        private async void Watcher_Received(BluetoothLEAdvertisementWatcher sender, BluetoothLEAdvertisementReceivedEventArgs args)
-        {
-            try {
-
-                IList<Guid> uuids = args.Advertisement.ServiceUuids;
-                // ushort identifier = args.Advertisement.ManufacturerData.First().CompanyId;
-                // byte[] data = args.Advertisement.ManufacturerData.First().Data.ToArray();
-
-                int count = uuids.Count;
-
-                Debug.WriteLine(" GOT DATA");
-
-            } catch
-            {
-                Debug.WriteLine("Exception!");
-            }
+        
 
             // I think this is the dispatcher that will send it to Unity
 
@@ -84,7 +56,6 @@ namespace BluetoothWatcher
             //    /* GPS Data Parsing / UI integration goes here */
             //}
             //);
-        }
-        
+
     }
 }
